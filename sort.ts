17aa5a22655b172fd25abd6502603dfbe2b24885
Niek van de Pas <niek_pas@live.nl>
2018-04-18 17:17:35 +0200

    Implement sorting method

--- sort.ts
+++ sort.ts
@@ -1,3 +1,13 @@
-export const sort = function(arr: Array<any>): Array<any> {
-    return [1, 2, 3];
+export const sort = function(arr: Array<any>, reverse: boolean = false): Array<any> {
+  // check if the array is all numbers
+  let isArrayOfNumbers = true;
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== "number") {
+      isArrayOfNumbers = false;
+      break;
+    }
+  }
+
+  const sortedArray = isArrayOfNumbers ? arr.sort((a, b) => a - b) : arr.sort() ;
+  return reverse ? sortedArray.reverse() : sortedArray;
 }
