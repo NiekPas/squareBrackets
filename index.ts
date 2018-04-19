7f35cd997abc3c41bdd5f6d3c8215be0f6c53272
Niek van de Pas <niek_pas@live.nl>
2018-04-19 15:42:08 +0200

    Fix import error due to earlier project restructuring

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-const sort = require('./sort');
-const reverse = require('./reverse');
-const split = require('./split');
-const isArray = require('./isArray');
-const isHomogeneous = require('./isHomogeneous');
-const isHeterogeneous = require('./isHeterogeneous');
+const sort = require('./lib/sort');
+const reverse = require('./lib/reverse');
+const split = require('./lib/split');
+const isArray = require('./lib/isArray');
+const isHomogeneous = require('./lib/isHomogeneous');
+const isHeterogeneous = require('./lib/isHeterogeneous');
 
 export = {sort, reverse, split, isArray, isHomogeneous, isHeterogeneous};
