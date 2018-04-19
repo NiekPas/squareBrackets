2885d8f4b29d05915130c87f093677a9c9ffb2f0
Niek van de Pas <niek_pas@live.nl>
2018-04-19 12:31:45 +0200

    Implement isArray polyfill

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 const sort = require('./sort');
 const reverse = require('./reverse');
 const split = require('./split');
+const isArray = require('./isArray');
 
-export = {sort, reverse, split};
+export = {sort, reverse, split, isArray};
