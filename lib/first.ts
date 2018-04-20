fe53088e530608ff25160eec0f2159dbbb961873
Niek van de Pas <niek_pas@live.nl>
2018-04-20 19:09:25 +0200

    Add documentation to first function

--- lib/first.ts
+++ lib/first.ts
@@ -1,5 +1,9 @@
 const isArray = require('./isArray');
-
+/**
+ * Returns the first element of an array. Equivalent to `arr[0]`.
+ * @param  {any[]} arr The array from which to take the first element
+ * @returns {any} The first element of `arr`.
+ */
 const first = function(arr: any[]): any {
   if (!isArray(arr)) {
     return arr;
