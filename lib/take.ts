b39bca8c6ed10fd9db82196266d0ac8946835fb6
Niek van de Pas <niek_pas@live.nl>
2018-04-20 19:45:14 +0200

    Write documentation for take

--- lib/take.ts
+++ lib/take.ts
@@ -1,5 +1,11 @@
 const isArray = require('./isArray');
-
+/**
+ * Takes `n` values from `arr`, by `arr`'s order of elements.
+ * If `n` is undefined, it takes one element.
+ * @param  {any[]} arr The array from which to take values.
+ * @param  {number} [n = 1]
+ * @returns {any} A single element (if `n === 1`) or an array of elements.
+ */
 const take = function(arr: any[], n: number = 1): any[] {
   if (!isArray(arr)) {
     return arr;
