057c44779a3c456f26eacb66ce8e763dd3866dcf
Niek van de Pas <niek_pas@live.nl>
2018-04-20 19:20:54 +0200

    Add documentation for reverse

--- lib/reverse.ts
+++ lib/reverse.ts
@@ -1,5 +1,9 @@
 const isArray = require('./isArray');
-
+/**
+ * Reverses an array, i.e. reorders its elements from last to first.
+ * @param  {any[]} arr The array to be reversed.
+ * @returns {any[]} The reversed array.
+ */
 const reverse = function(arr: any[]): any[] {
   if (!isArray(arr)) {
     return arr;
