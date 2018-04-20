3b3f50a0902dd690e0400cf46a09fe7299aebae7
Niek van de Pas <niek_pas@live.nl>
2018-04-20 19:24:19 +0200

    Write documentation for sample

--- lib/sample.ts
+++ lib/sample.ts
@@ -1,5 +1,15 @@
 const isArray = require('./isArray');
-
+/**
+ * Takes a (set of) random element(s) from `arr`.
+ * If `numberOfElements` is undefined, it defaults to `1`, returning a single randomly picked element.
+ * If `numberOfElements` is greater than 1, an array is returned.
+ * Note: This function does not exclude already picked values,
+ * i.e. it may return the same value several times.
+ * See also {@link sampleDistinct}.
+ * @param  {any[]} arr The array from which to be sampled.
+ * @param  {number=1} numberOfElements
+ * @returns {any} Either a single randomly picked element, or an array of randomly picked elements.
+ */
 const sample = function(arr: any[], numberOfElements: number = 1): any {
   const sampledElements: any[] = [];
   if (!isArray(arr)) {
