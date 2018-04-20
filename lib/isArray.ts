5dd31f0d62ac8a8ec668981d9047587f356fa1a8
Niek van de Pas <niek_pas@live.nl>
2018-04-20 19:12:18 +0200

    Add documentation for isArray function

--- lib/isArray.ts
+++ lib/isArray.ts
@@ -1,3 +1,7 @@
+/** Checks whether the input value is an array. Polyfill for ES6 `Array.isArray()`.
+ * @param {any} val The value to be checked.
+ * @returns {boolean} A boolean indicating whether the value is an array.
+ */
 const isArray = function(val: any): boolean {
   return Object.prototype.toString.call(val) === '[object Array]';
 };
