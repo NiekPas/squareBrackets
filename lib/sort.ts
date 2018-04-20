a2367422c2b03286a41a9c70c4992cc1e2151089
Niek van de Pas <niek_pas@live.nl>
2018-04-20 19:26:52 +0200

    Write documentation for sort

--- lib/sort.ts
+++ lib/sort.ts
@@ -1,5 +1,12 @@
 const isArray = require('./isArray');
-
+/**
+ * Sorts an array. This function differs from `Array.prototype.sort()`
+ * in that it sorts numbers correctly (that is, arithmetically rather than by comparing string values).
+ * If `arr` is not an array of numbers, this function calls `Array.prototype.sort()`.
+ * @param  {any[]} arr The array to be sorted.
+ * @param  {boolean} [reverse = false] Indicates whether the array should be reversed after sorting.
+ * @returns {any[]} The sorted array, reversed if so indicated.
+ */
 const sort = function(arr: any[], reverse: boolean = false): any[] {
   if (!isArray(arr)) {
     return arr;
