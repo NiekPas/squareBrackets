f292de958198c699c3d4def61288695a2db838e5
Niek van de Pas <niek_pas@live.nl>
2018-04-20 19:18:21 +0200

    Document isHomogeneous and isHeterogeneous functions

--- lib/isHeterogeneous.ts
+++ lib/isHeterogeneous.ts
@@ -1,6 +1,13 @@
 const isArray = require('./isArray');
 const isHomogeneous = require('./isHomogeneous');
-
+/**
+ * Checks whether the elements of an array differ in type,
+ * evaluating only a single level of nesting (see {@link isDeeplyHeterogeneous}.
+ * This function is the inverse of {@link isHomogeneous}.
+ * @param  {any[]} arr The array to be evaluated.
+ * @returns boolean A boolean indicating whether the array is heterogeneous,
+ * or `false` if the input value is not an array.
+ */
 const isHeterogeneous = function(arr: any[]): boolean {
   if (!isArray(arr)) {
     return false;
--- lib/isHomogeneous.ts
+++ lib/isHomogeneous.ts
@@ -1,5 +1,12 @@
 const isArray = require('./isArray');
-
+/**
+ * Checks whether all elements of an array are of the same type,
+ * evaluating only a single level of nesting (see {@link isDeeplyHomogeneous}.
+ * This function is the inverse of {@link isHeterogeneous}.
+ * @param  {any[]} arr The array to be evaluated.
+ * @returns boolean A boolean indicating whether the array is Homogeneous,
+ * or `false` if the input value is not an array.
+ */
 const isHomogeneous = function(arr: any[]): boolean {
   if (!isArray(arr)) {
     return false;
