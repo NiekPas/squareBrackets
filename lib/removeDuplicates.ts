c86bad9d679ff408dd9a1a881ae924117eb50984
Niek van de Pas <niek_pas@live.nl>
2018-04-23 11:36:40 +0200

    Write documentation for removeDuplicates

--- lib/removeDuplicates.ts
+++ lib/removeDuplicates.ts
@@ -1,7 +1,23 @@
 const isArray = require('./isArray');
 // tslint:disable-next-line:variable-name
 const _areEqual = require('./_areEqual');
-
+/**
+ * Removes all duplicates from an array.
+ * Two elements are considered equal if they are they same value type,
+ * both arrays with equal elements,
+ * or both objects with identical key/value pairs (the order of which does not matter).
+ * Two functions are considered equal if and only if they refer to the same function in memory (reference equality).
+ * When two elements are duplicate, only the first is retained.
+ *
+ * `squareBrackets.removeDuplicates([1, 1, 2 ,3, 2]);`<br>
+ * `// [1, 2, 3]`<br>
+ * `squareBrackets.removeDuplicates([[1, 2, 3], [1, 2, 3], [4, 5, 6]]);`<br>
+ * `// [[1, 2, 3], [4, 5, 6]]`<br>
+ * `squareBrackets.removeDuplicates({name: 'Daniel', actor: true}, {actor: true, name: 'Daniel'});`<br>
+ * `// { name: 'Daniel', actor: true }`<br>
+ * @param  {any[]} arr The array from which to remove duplicates.
+ * @returns {any} The original array without its duplicates.
+ */
 const removeDuplicates = function(arr: any[]): any[] {
   if (!isArray(arr)) {
     return arr;
